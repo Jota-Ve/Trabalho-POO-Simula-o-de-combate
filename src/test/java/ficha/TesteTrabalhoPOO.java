2ea3452a0cc77d6498b12f90748b2101920e140d
Nayara Dornelas <85142999+NayaraDornelaas@users.noreply.github.com>
2025-10-06 15:37:15 +0000

    refatoracao testes testeTrabalhoPOO

--- src/test/java/ficha/TesteTrabalhoPOO.java
+++ src/test/java/ficha/TesteTrabalhoPOO.java
@@ -21,58 +21,37 @@ public class TesteTrabalhoPOO {
 
     private static final PrintStream saidaOriginal = System.out;
     private static final ByteArrayOutputStream saidaCapturada = new ByteArrayOutputStream();
- 
+    private static String saidaTestes;
+
     @BeforeAll
-    public static void inicializa() {
-        
+    public static void inicializa() throws Exception {
         System.setOut(new PrintStream(saidaCapturada));
-    }
 
-    @AfterAll
-    public static void finaliza() {
-        System.setOut(saidaOriginal);
-    }
+        Raca racaMock = mock(Raca.class);
+        when(racaMock.getNome()).thenReturn("Elfo");
 
-    @Test
-    void testImprimirFichaProvisoria() {
-        Raca racaFalsa = mock(Raca.class);
-        when(racaFalsa.getNome()).thenReturn("Elfo");
+        Classe classeMock = mock(Classe.class);
+        when(classeMock.getNome()).thenReturn("Guerreiro");
 
-        Classe classeFalsa = mock(Classe.class);
-        when(classeFalsa.getNome()).thenReturn("Guerreiro");
-
-        try (MockedStatic<Personagem> personagemFalso = Mockito.mockStatic(Personagem.class)) {
-            personagemFalso.when(Personagem::getNome).thenReturn("Arwen");
-            personagemFalso.when(Personagem::getRaca).thenReturn(racaFalsa);
-            personagemFalso.when(Personagem::getClasse).thenReturn(classeFalsa);
-            personagemFalso.when(Personagem::getNivel).thenReturn(5);
-
-            personagemFalso.when(Personagem::imprimeAtributos).thenAnswer(invocacao -> {
+        try (MockedStatic<Personagem> personagemMock = Mockito.mockStatic(Personagem.class)) {
+            personagemMock.when(Personagem::getNome).thenReturn("Arwen");
+            personagemMock.when(Personagem::getRaca).thenReturn(racaMock);
+            personagemMock.when(Personagem::getClasse).thenReturn(classeMock);
+            personagemMock.when(Personagem::getNivel).thenReturn(5);
+            personagemMock.when(Personagem::imprimeAtributos).thenAnswer(invocacao -> {
                 System.out.println("Força: 10  Agilidade: 12  Inteligência: 8");
                 return null;
             });
-
             TrabalhoPOO.imprimirFichaProvisoria();
-
-            String textoImpresso = saidaCapturada.toString();
-
-            assertTrue(textoImpresso.contains("Nome: Arwen"));
-            assertTrue(textoImpresso.contains("Raca: Elfo"));
-            assertTrue(textoImpresso.contains("Classe: Guerreiro"));
-            assertTrue(textoImpresso.contains("Nivel: 5"));
-            assertTrue(textoImpresso.contains("Força: 10"));
         }
-    }
 
-    @Test
-    void testaImprimirFicha() throws Exception {
-        Raca racaFalsa = mock(Raca.class);
-        when(racaFalsa.getNome()).thenReturn("Anao");
-        when(racaFalsa.getPoder()).thenReturn(1);
+        racaMock = mock(Raca.class);
+        when(racaMock.getNome()).thenReturn("Anao");
+        when(racaMock.getPoder()).thenReturn(1);
 
-        Classe classeFalsa = mock(Classe.class);
-        when(classeFalsa.getNome()).thenReturn("Bruxo");
-        when(classeFalsa.getPoder()).thenReturn(2);
+        classeMock = mock(Classe.class);
+        when(classeMock.getNome()).thenReturn("Bruxo");
+        when(classeMock.getPoder()).thenReturn(2);
 
         Map<Integer, String> mapaDePoderes = new HashMap<>();
         mapaDePoderes.put(1, "Poder de Raça: Ataque especial");
@@ -82,37 +61,104 @@ public class TesteTrabalhoPOO {
         campoMapa.setAccessible(true);
         campoMapa.set(null, mapaDePoderes);
 
-        try (MockedStatic<Personagem> personagemFalso = Mockito.mockStatic(Personagem.class)) {
-            personagemFalso.when(Personagem::getNome).thenReturn("Zé");
-            personagemFalso.when(Personagem::getNivel).thenReturn(10);
-            personagemFalso.when(Personagem::getRaca).thenReturn(racaFalsa);
-            personagemFalso.when(Personagem::getClasse).thenReturn(classeFalsa);
-            personagemFalso.when(Personagem::getPv).thenReturn(80);
-            personagemFalso.when(Personagem::getMana).thenReturn(120);
-            personagemFalso.when(Personagem::getAtq).thenReturn(15);
-            personagemFalso.when(Personagem::getDefesa).thenReturn(6);
-            personagemFalso.when(Personagem::getDeslocamento).thenReturn(7);
-
-            personagemFalso.when(Personagem::imprimeAtributos).thenAnswer(invocacao -> {
+        try (MockedStatic<Personagem> personagemMock = Mockito.mockStatic(Personagem.class)) {
+            personagemMock.when(Personagem::getNome).thenReturn("Zé");
+            personagemMock.when(Personagem::getNivel).thenReturn(10);
+            personagemMock.when(Personagem::getRaca).thenReturn(racaMock);
+            personagemMock.when(Personagem::getClasse).thenReturn(classeMock);
+            personagemMock.when(Personagem::getPv).thenReturn(80);
+            personagemMock.when(Personagem::getMana).thenReturn(120);
+            personagemMock.when(Personagem::getAtq).thenReturn(15);
+            personagemMock.when(Personagem::getDefesa).thenReturn(6);
+            personagemMock.when(Personagem::getDeslocamento).thenReturn(7);
+            personagemMock.when(Personagem::imprimeAtributos).thenAnswer(invocacao -> {
                 System.out.println("Força: 6  Agilidade: 14  Inteligência: 18");
                 return null;
             });
-
             TrabalhoPOO.imprimirFicha();
-
-            String textoImpresso = saidaCapturada.toString();
-
-            assertTrue(textoImpresso.contains("Nome: Zé"));
-            assertTrue(textoImpresso.contains("Raca: Anao"));
-            assertTrue(textoImpresso.contains("Classe: Bruxo"));
-            assertTrue(textoImpresso.contains("Nivel: 10"));
-            assertTrue(textoImpresso.contains("PV: 80"));
-            assertTrue(textoImpresso.contains("Mana: 120"));
-            assertTrue(textoImpresso.contains("Ataque: 15"));
-            assertTrue(textoImpresso.contains("Defesa: 6"));
-            assertTrue(textoImpresso.contains("Deslocamento: 7"));
-            assertTrue(textoImpresso.contains("Poder de Raça: Ataque especial"));
-            assertTrue(textoImpresso.contains("Poder de Classe: Bola de Fogo"));
         }
+        
+        saidaTestes = saidaCapturada.toString();
+    }
+
+    @AfterAll
+    public static void finaliza() {
+        System.setOut(saidaOriginal);
+    }
+    
+    @Test
+    void testNomeFichaProvisoria() {
+        assertTrue(saidaTestes.contains("Nome: Arwen"));
+    }
+    
+    @Test
+    void testRacaFichaProvisoria() {
+        assertTrue(saidaTestes.contains("Raca: Elfo"));
+    }
+    
+    @Test
+    void testClasseFichaProvisoria() {
+        assertTrue(saidaTestes.contains("Classe: Guerreiro"));
+    }
+
+    @Test
+    void testNivelFichaProvisoria() {
+        assertTrue(saidaTestes.contains("Nivel: 5"));
+    }
+
+    @Test
+    void testAtributosFichaProvisoria() {
+        assertTrue(saidaTestes.contains("Força: 10  Agilidade: 12  Inteligência: 8"));
+    }
+
+    @Test
+    void testNomeFichaCompleta() {
+        assertTrue(saidaTestes.contains("Nome: Zé"));
+    }
+    
+    @Test
+    void testRacaFichaCompleta() {
+        assertTrue(saidaTestes.contains("Raca: Anao"));
+    }
+    
+    @Test
+    void testClasseFichaCompleta() {
+        assertTrue(saidaTestes.contains("Classe: Bruxo"));
+    }
+
+    @Test
+    void testNivelFichaCompleta() {
+        assertTrue(saidaTestes.contains("Nivel: 10"));
+    }
+    
+    @Test
+    void testPVFichaCompleta() {
+        assertTrue(saidaTestes.contains("PV: 80"));
+    }
+
+    @Test
+    void testManaFichaCompleta() {
+        assertTrue(saidaTestes.contains("Mana: 120"));
+    }
+    
+    @Test
+    void testAtaqueEDefesaFichaCompleta() {
+        assertTrue(saidaTestes.contains("Ataque: 15"));
+        assertTrue(saidaTestes.contains("Defesa: 6"));
+    }
+    
+    @Test
+    void testDeslocamentoFichaCompleta() {
+        assertTrue(saidaTestes.contains("Deslocamento: 7"));
+    }
+    
+    @Test
+    void testPoderDeRacaFichaCompleta() {
+        assertTrue(saidaTestes.contains("Poder de Raça: Ataque especial"));
+    }
+    
+    @Test
+    void testPoderDeClasseFichaCompleta() {
+        assertTrue(saidaTestes.contains("Poder de Classe: Bola de Fogo"));
     }
 }
